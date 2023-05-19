267ac59d9ce824239081feec1a017737e3c9c811
Patrick Schilf <pschilf@unb.ca>
2023-05-19 13:22:43 -0300

    Move session middleware logic to composable

--- composables/session.ts
+++ composables/session.ts
@@ -1,7 +1,17 @@
-export function useCurrentSession() {
+import { SessionData } from "h3"
+
+export async function useCurrentSession() {
   const { name } = useSessionConfig()
-  const sessionId = useState(name)
-  return readonly(sessionId)
+  const { data: session, refresh } = await useFetch<SessionData>(`/api/session`, {
+    headers: {
+      Cookie: `${name}=${useCookie(name)}`,
+    },
+  })
+
+  return {
+    session: readonly(session),
+    refresh,
+  }
 }
 
 export function useSessionConfig() {
