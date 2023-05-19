e1560458386580af180b83195a55f3d835d0cc52
Patrick Schilf <pschilf@unb.ca>
2023-05-19 10:59:09 -0300

    Build playground

---
+++ playground/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>unb-libraries/nuxt-layers/base</h1>
+  <section>
+    <h2>Current session</h2>
+    <pre>{{ session }}</pre>
+  </section>
+</template>
+
+<script setup lang="ts">
+const session = useCurrentSession()
+</script>
